22a390065199f2de55c9450c4bb4370684743afb
Michał Kwiatkowski <110732078+Kfiatek430@users.noreply.github.com>
2025-03-07 11:46:39 +0100

    fix: correct typo in Navbar (#3)

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -13,7 +13,7 @@
       />
       <span
         class="self-center text-2xl font-semibold whitespace-nowrap text-white"
-        >Coinfly</span
+        >Coinfy</span
       >
     </a>
     <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
