cde3b93af97d55e227507831120db575edde5f74
Michał <110732078+Kfiatek430@users.noreply.github.com>
2025-03-14 11:48:42 +0100

    feat: add Currency type and apply it in SharedService, format response

--- src/shared/share.service.ts
+++ src/shared/share.service.ts
@@ -1,11 +1,12 @@
 import axios, { type AxiosResponse } from "axios";
 import { StaticConfig } from "../static/config";
+import type { Currency } from "../interfaces/currency";
 
 export class SharedService {
   constructor() {}
   async getTables() {
     try {
-      let data: Array<unknown> = [];
+      let data: Array<Currency> = [];
       StaticConfig.tablesNames.forEach(async (e) => {
         const response: AxiosResponse<
           [
@@ -13,16 +14,19 @@ export class SharedService {
               table: string;
               no: string;
               effectiveDate: Date;
-              rates: Array<{
-                currency: string;
-                code: string;
-                mid: number;
-              }>;
+              rates: Array<Currency>;
             }
           ]
         > = await axios.get(`${StaticConfig.getTablesUrl}/${e}?format=json`);
         const responseData = response.data;
         data.push(...responseData[0].rates);
+        data.forEach(currency => {
+          let curr = currency.currency
+
+          if (curr.indexOf("(") != -1) {
+            currency.currency = curr.slice(0, curr.indexOf("(") - 1)
+          }
+        })
       });
 
       return data;
