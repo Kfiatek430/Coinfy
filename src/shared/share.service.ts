e263ec713e6e2d19d059ee9deb3effc84bfa2b7d
Michał <110732078+Kfiatek430@users.noreply.github.com>
2025-03-21 11:55:25 +0100

    fix(SharedService): fix issue with fetching multiple tables

--- src/shared/share.service.ts
+++ src/shared/share.service.ts
@@ -1,37 +1,50 @@
-import axios, { type AxiosResponse } from "axios";
-import { StaticConfig } from "../static/config";
-import type { Currency } from "../interfaces/currency";
+  import axios, { type AxiosResponse } from "axios";
+  import { StaticConfig } from "../static/config";
+  import type { Currency } from "../interfaces/currency";
 
-export class SharedService {
-  constructor() {}
-  async getTables() {
-    try {
-      let data: Array<Currency> = [];
-      StaticConfig.tablesNames.forEach(async (e) => {
-        const response: AxiosResponse<
-          [
-            {
-              table: string;
-              no: string;
-              effectiveDate: Date;
-              rates: Array<Currency>;
-            }
-          ]
-        > = await axios.get(`${StaticConfig.getTablesUrl}/${e}?format=json`);
-        const responseData = response.data;
-        data.push(...responseData[0].rates);
+  export class SharedService {
+    constructor() {}
+    async getTables() {
+      try {
+        let data: Currency[] = [];
+
+        let responses = await Promise.all(
+            StaticConfig.tablesNames.map(async (e) => {
+              const response: AxiosResponse<
+                [
+                  {
+                    table: string;
+                    no: string;
+                    effectiveDate: Date;
+                    rates: Currency[];
+                  }
+                ]
+                > = await axios.get(`${StaticConfig.getTablesUrl}/${e}?format=json`);
+              
+              return response.data[0].rates
+            }  
+          )
+        )
+        
+        data = responses.flat()
+        
         data.forEach(currency => {
-          let curr = currency.currency
-
-          if (curr.indexOf("(") != -1) {
-            currency.currency = curr.slice(0, curr.indexOf("(") - 1)
+          let curr = currency.currency;
+          if (curr.includes("(")) {
+            currency.currency = curr.slice(0, curr.indexOf("(") - 1);
           }
-        })
-      });
 
-      return data;
-    } catch (e) {
-      console.error(e);
+          currency.currency = currency.currency
+            .trim()
+            .split(" ")
+            .map(el => el[0].toLocaleUpperCase() + el.slice(1))
+            .join(" ");
+        });
+
+        return data;
+      } catch (e) {
+        console.error(e);
+        return []
+      }
     }
   }
-}
