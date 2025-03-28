328326c53ee00943eb707f49c0dcd3f459937587
radek1248 <rbrunowski@wp.pl>
2025-03-28 11:45:59 +0100

    feat: set currency chart

--- src/components/CurrencyChart.svelte
+++ src/components/CurrencyChart.svelte
@@ -1,71 +1,131 @@
 <script>
-  import * as d3 from 'd3';
-  
-  let data = [
-    { date: '2025-01-01', value: 1.1 },
-    { date: '2025-01-02', value: 1.2 },
-    { date: '2025-01-03', value: 1.15 },
-    { date: '2025-01-04', value: 1.3 },
-    { date: '2025-01-05', value: 1.25 }
-  ];
+  // @ts-ignore
+  import * as d3 from "d3";
+  import { SharedService } from "./../shared/share.service";
+  import { onMount } from "svelte";
 
-  const margin = { top: 20, right: 30, bottom: 40, left: 40 };
-  const width = 800 - margin.left - margin.right;
-  const height = 400 - margin.top - margin.bottom;
+  const shared = new SharedService();
+
+  async function fetchCurrency() {
+    const tables = await shared.getTables();
+    console.log(tables[0]);
+    const currency = await shared.getRates(
+      tables[0],
+      new Date("2024-01-01"),
+      new Date("2025-01-01")
+    );
+    return currency;
+  }
+
+  /**
+   * @type {{ date: string; value: number; }[]}
+   */
+  let data = [];
+
+  async function prepareData() {
+    const currency = await fetchCurrency();
+    currency.forEach(element => {
+      data.push({ date: element.effectiveDate, value: element.mid })
+    })
+  }
+
+  /**
+   * @type {{ left: any; right: any; top: any; bottom: any; }}
+   */
+  let margin;
+  /**
+   * @type {number}
+   */
+  let width;
+  /**
+   * @type {number}
+   */
+  let height; 
+  /**
+   * @type {(arg0: any) => any}
+   */
+  let xScale;
+  /**
+   * @type {(arg0: any) => any}
+   */
+  let yScale;
+  /**
+   * @type {any}
+   */
+  let line;
+
+  async function setChart() {
+    await prepareData();
+    margin = { top: 20, right: 30, bottom: 40, left: 40 };
+    width = 800 - margin.left - margin.right;
+    height = 400 - margin.top - margin.bottom;
+
+    const parseDate = d3.timeParse("%Y-%m-%d");
+    data.forEach((d) => {
+      d.date = parseDate(d.date);
+    });
+
+    xScale = d3
+      .scaleTime()
+      .domain(d3.extent(data, (/** @type {{ date: any; }} */ d) => d.date))
+      .range([0, width]);
+
+    yScale = d3
+      .scaleLinear()
+      .domain([
+        d3.min(data, (/** @type {{ value: any; }} */ d) => d.value) - 0.1,
+        d3.max(data, (/** @type {{ value: any; }} */ d) => d.value) + 0.1,
+      ])
+      .range([height, 0]);
+
+    line = d3
+      .line()
+      .x((/** @type {{ date: any; }} */ d) => xScale(d.date))
+      .y((/** @type {{ value: any; }} */ d) => yScale(d.value));
+  }
 
   let svg;
-  let xScale, yScale, line;
 
-  const parseDate = d3.timeParse('%Y-%m-%d');
-  data.forEach(d => { d.date = parseDate(d.date); });
+  onMount(async () => {
+      await setChart();
 
-  xScale = d3.scaleTime()
-    .domain(d3.extent(data, d => d.date))
-    .range([0, width]);
+      svg = d3
+        .select("#chart")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  yScale = d3.scaleLinear()
-    .domain([d3.min(data, d => d.value) - 0.1, d3.max(data, d => d.value) + 0.1])
-    .range([height, 0]);
+      svg
+        .append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(xScale));
 
-  line = d3.line()
-    .x(d => xScale(d.date))
-    .y(d => yScale(d.value));
+      svg.append("g").call(d3.axisLeft(yScale));
 
-  import { onMount } from 'svelte';
-  onMount(() => {
-    svg = d3.select('#chart')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
-    
-    svg.append('g')
-      .attr('transform', `translate(0,${height})`)
-      .call(d3.axisBottom(xScale));
+      svg
+        .append("path")
+        .data([data])
+        .attr("fill", "none")
+        .attr("stroke", "blue")
+        .attr("stroke-width", 2)
+        .attr("d", line);
 
-    svg.append('g')
-      .call(d3.axisLeft(yScale));
+      svg
+        .append("text")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 10)
+        .style("text-anchor", "middle")
+        .text("Date");
 
-    svg.append('path')
-      .data([data])
-      .attr('fill', 'none')
-      .attr('stroke', 'blue')
-      .attr('stroke-width', 2)
-      .attr('d', line);
-
-    svg.append('text')
-      .attr('x', width / 2)
-      .attr('y', height + margin.bottom - 10)
-      .style('text-anchor', 'middle')
-      .text('Date');
-
-    svg.append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('x', -height / 2)
-      .attr('y', -margin.left + 15)
-      .style('text-anchor', 'middle')
-      .text('Currency Rate');
-  });
+      svg
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height / 2)
+        .attr("y", -margin.left + 15)
+        .style("text-anchor", "middle")
+        .text("Currency Rate");
+    });
 </script>
 
 <style>
