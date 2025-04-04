7ea94e80e1b3eb56457bb063cfd4017c4fee7aec
Michał Kwiatkowski <110732078+Kfiatek430@users.noreply.github.com>
2025-04-04 08:37:14 +0200

    fix: minor fixes (#20)
    * fix(GoldConverter): typo
    
    * fix: add missing packages
    
    * fix(About): fix background

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,5 +1,9 @@
 <div class="relative">
-  <div class="relative px-4 py-8 bg-blue-900 min-h-[100vh] text-white ">
+  <div
+  class="absolute inset-0 bg-gradient-to-b from-blue-900 from-80% to-white to-30%"
+  ></div>
+
+  <div class="relative px-4 py-8 min-h-[100vh] text-white ">
     <h1
       class="relative pt-15 mb-4 z-10 text-4xl text-center leading-none tracking-tight md:text-4xl lg:text-5xl text-white"
     >
